09edb3b331673929288f24fe30863853b5fe7706
Gabe Cook <gabe565@gmail.com>
2024-06-24 16:41:42 -0500

    perf(redis): Implement `io.WriterTo` to remove an allocation during cache fetch

--- internal/redis/reader.go
+++ internal/redis/reader.go
@@ -36,6 +36,27 @@ func (c *CacheReader) Read(p []byte) (int, error) {
 	return c.buf.Read(p)
 }
 
+var _ io.WriterTo = &CacheReader{}
+
+func (c *CacheReader) WriteTo(w io.Writer) (int64, error) {
+	var n int64
+	for c.chunk < c.chunks {
+		key := c.key + "_body_" + strconv.Itoa(c.chunk)
+		b, err := c.cache.Do(c.ctx, c.cache.B().Get().Key(key).Build()).AsBytes()
+		if err != nil {
+			return n, err
+		}
+
+		chunkN, err := w.Write(b)
+		n += int64(chunkN)
+		if err != nil {
+			return n, err
+		}
+		c.chunk++
+	}
+	return n, nil
+}
+
 func (c *CacheReader) Close() error {
 	locks.Unlock(c.key)
 	return nil
