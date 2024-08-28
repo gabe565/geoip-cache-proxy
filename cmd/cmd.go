3634e80519df45637b289827ee41cbf0d52d7df2
Gabe Cook <gabe565@gmail.com>
2024-08-28 02:26:38 -0500

    chore(cmd): Clean up versioning using a functional argument

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -14,28 +14,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "beta"
-
-func New() *cobra.Command {
-	version, commit := buildVersion(version)
-
+func New(opts ...Option) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:         "geoip-cache-proxy",
-		Short:       "A GeoIP database caching proxy",
-		RunE:        run,
-		Version:     version,
-		Annotations: map[string]string{"commit": commit},
+		Use:   "geoip-cache-proxy",
+		Short: "A GeoIP database caching proxy",
+		RunE:  run,
 
 		ValidArgsFunction: cobra.NoFileCompletions,
 		SilenceErrors:     true,
 		SilenceUsage:      true,
 		DisableAutoGenTag: true,
 	}
-	cmd.InitDefaultVersionFlag()
 	conf := config.NewDefault()
 	conf.RegisterFlags(cmd)
 	config.RegisterCompletions(cmd)
 	cmd.SetContext(config.NewContext(context.Background(), conf))
+
+	for _, opt := range opts {
+		opt(cmd)
+	}
+
 	return cmd
 }
 
@@ -51,7 +49,10 @@ func run(cmd *cobra.Command, _ []string) error {
 		return err
 	}
 
-	slog.Info("GeoIP caching proxy", "version", version, "commit", cmd.Annotations["commit"])
+	slog.Info("GeoIP caching proxy",
+		"version", cmd.Annotations[VersionKey],
+		"commit", cmd.Annotations[CommitKey],
+	)
 
 	ctx, cancel := signal.NotifyContext(cmd.Context(), os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
 	defer cancel()
--- main.go
+++ main.go
@@ -8,9 +8,12 @@ import (
 	"github.com/gabe565/geoip-cache-proxy/internal/config"
 )
 
+var version = "beta"
+
 func main() {
 	config.InitLog(os.Stderr, slog.LevelInfo, config.FormatAuto)
-	if err := cmd.New().Execute(); err != nil {
+	root := cmd.New(cmd.WithVersion(version))
+	if err := root.Execute(); err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
 	}
