f88ee820e0f70def9b314a4cc2c96e3008104122
Gabe Cook <gabe565@gmail.com>
2024-11-13 22:00:43 -0600

    feat: Set custom User-Agent header

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log/slog"
+	"net/http"
 	"os"
 
 	"gabe565.com/geoip-cache-proxy/cmd"
 	"gabe565.com/geoip-cache-proxy/internal/config"
 	"gabe565.com/utils/cobrax"
+	"gabe565.com/utils/httpx"
 )
 
 var version = "beta"
@@ -14,6 +16,7 @@ var version = "beta"
 func main() {
 	config.InitLog(os.Stderr, slog.LevelInfo, config.FormatAuto)
 	root := cmd.New(cobrax.WithVersion(version))
+	http.DefaultTransport = httpx.NewUserAgentTransport(nil, cobrax.BuildUserAgent(root))
 	if err := root.Execute(); err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
